c50859604e2b0f911494abd08d2968a8d45d9350
akash-skillstreet <akash.g@skillstreet.io>
2024-03-07 11:38:34 +0530

    Version 2

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. 
         </p>
+        <h1>This is Version 2</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
